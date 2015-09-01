1d121dc42906fc58fa3a7febff8dabd29fba34f2
tsukimizake <shomasd@gmail.com>
2015-09-01 20:04:48 +0900

    add license to company-dcd-helm-file-type.el.

--- company-dcd-helm-file-type.el
+++ company-dcd-helm-file-type.el
@@ -1,7 +1,21 @@
 
 ;;; Commentary:
 ;; use helm's obsolete type-attribute `file-line'.
-;; This code is from http://rubikitch.com/f/mylisp-helm-file-line.el
+;; This code is from http://rubikitch.com/f/mylisp-helm-file-line.el , which is released as a free software.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 ;;; Code:
 
